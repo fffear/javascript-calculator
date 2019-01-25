4115374af5fedb1c85d720ef67623d1911f1985b
Kevin Vuong <c_k.k@hotmail.com>
2019-01-26 07:02:02 +1100

    add operate function

--- jscript.js
+++ jscript.js
@@ -1,15 +1,23 @@
 function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+      return a - b;
+  }
+  
+  function multiply(a, b) {
+      return a * b;
+  }
+  
+  function divide(a, b) {
+      return a / b;
+  }
+  
+  function operate(a, operator, b) {
+    return operator == "+" ? add(a, b)
+    : operator == "-" ? subtract(a, b)
+    : operator == "*" ? multiply(a, b)
+    : operator == "/" ? divide(a, b)
+    : "error";
 }
